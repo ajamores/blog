0e0261b4afa3e3969a07534830b84d2c607938b2
Armand Amores <aj_amores@hotmail.com>
2026-03-19 19:53:52 -0400

    - added security
    - add helmet, rate limiter, and cors protection

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ import express from 'express';
 import { config } from 'dotenv';
 import { connectDB, disconnectDB } from '../src/config/db.js';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import cors from 'cors';
+
 
 //start app/configs
 const app = express();
@@ -9,10 +13,31 @@ const PORT = 8080;
 config();
 connectDB();
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100
+})
 
+const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 10 // only 10 login attempts per 15 min
+})
 
 
 //-------------MiddleWares---------------
+
+//Rate limiter
+app.use(limiter);
+
+// protect against web vulnerabilities by setting HTTP headers
+app.use(helmet);
+
+//CORS
+app.use(cors({
+    origin: "http://localhost:3000", //  frontend URL
+    credentials: true
+}))
+
 //dont forget express comes with build in middle ware like json
 app.use(express.json()); // parses JSON bodies first
 
@@ -28,12 +53,14 @@ app.use((req, res, next) => {
 
 
 
+
 //------------Route Endpoints--------------------\
 import authRoutes from './routes/authRoutes.js';
 import blogRoutes from './routes/blogRoutes.js';
 import categoryRoutes from './routes/categoryRoutes.js'
 
-app.use("/auth", authRoutes);
+
+app.use("/auth", authLimiter ,authRoutes);
 app.use("/blog", blogRoutes);
 app.use("/category", categoryRoutes);
 
