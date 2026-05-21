9c95ffa4ba2448ef85df1834c25437114ef04f03
Armand Amores <aj_amores@hotmail.com>
2026-05-20 20:08:43 -0400

    -increase rate limit from 100 to 300

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ connectDB();
 //limits every request to your server — static files, API calls, page loads, everything — to 100 requests per 15 minutes per IP
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100
+    max: 300
 })
 
 //limit requests for logging in 
